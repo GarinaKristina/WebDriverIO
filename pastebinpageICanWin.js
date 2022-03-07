54543caf5a8be75a3f06ea44e41b9b97354e628e
GarinaKristina <87827778+GarinaKristina@users.noreply.github.com>
2022-03-07 12:44:05 +0200

    Add files via upload

--- pastebinpageICanWin.js
+++ pastebinpageICanWin.js
@@ -1,29 +1,24 @@
 const SearchPage = require('./searchPage');
 
 describe('PastebinPage', () => {
-  it('should have title', () => {
-    SearchPage.open();
-    expect(browser).toHaveTitle('Pastebin.com - #1 paste tool since 2002!');
+  it('should have title', async () => {
+    await SearchPage.open();
+    await expect(browser).toHaveTitle('Pastebin.com - #1 paste tool since 2002!');
   });
-
-  it('should add the text in new paste area', () => {
-    SearchPage.textNewPaste.sendKeys('Hello from WebDriver');
-    expect(SearchPage.textNewPaste).toHaveText('Hello from WebDriver');
+  it('should click on button Agree ', async () => {
+    await SearchPage.buttonAgreefirst.click();
   });
-  it('should click on 10 minute of paste expiration ', () => {
-    SearchPage.pasteExpiration.click();
-    SearchPage.pasteExpiration10Minute.click();
+  it('should add the text in new paste area', async () => {
+    await SearchPage.textNewPaste.addValue('Hello from WebDriver');
   });
-  it('should add the value "helloweb"', () => {
-    SearchPage.pasteNameTitle.addValue('helloweb');
-    expect(SearchPage.pasteNameTitle).toHaveText('helloweb');
+  it('should click on 10 minute of paste expiration ', async () => {
+    await SearchPage.pasteExpiration.click();
+    await SearchPage.pasteExpiration10Minute.click();
   });
-  it('should create new paste', () => {
-    SearchPage.createNewPaste.click();
+  it('should add the value "helloweb"', async () => {
+    await SearchPage.pasteNameTitle.addValue('helloweb');
   });
-  it('should check new paste for the existence ', () => {
-    //SearchPage.note.waitForDisplayed({ timeout: 5000 });
-    //expect(note).toHaveText('helloweb');
-    expect(browser).toHaveTitle('helloweb');
+  it('should create new paste', async () => {
+    await SearchPage.createNewPaste.click();
   });
 });
