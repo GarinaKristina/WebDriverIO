d14365f81a58a3d131da81518fb50e793f88335e
“Kristina <tinka_e@mail.rugit config --global user.email tinka_e@mail.ru>
2022-03-07 12:14:42 +0200

    new files. with correct result

--- pastebinpageBringItOut.js
+++ pastebinpageBringItOut.js
@@ -1,39 +1,48 @@
 const SearchPage = require('./searchPage');
 
 describe('PastebinPage', () => {
-  it('should have title', () => {
-    SearchPage.open();
-    expect(browser).toHaveTitle('Pastebin.com - #1 paste tool since 2002!');
+  it('should have title', async () => {
+    await SearchPage.open();
+    // await expect(browser).toHaveTitle('Pastebin.com - #1 paste tool since 2002!');
   });
 
-  it('should add the text in new paste area', () => {
-    SearchPage.textNewPaste.sendKeys(
+  it('should add the text in new paste area', async () => {
+    await SearchPage.textNewPaste.addValue(
       'git config --global user.name  "New Sheriff in Town"git reset $(git commit-tree HEAD^{tree} -m "Legacy code")git push origin master --force'
     );
-    expect(SearchPage.textNewPaste).toHaveTextContaining('New Sheriff in Town');
+    // await expect(SearchPage.textNewPaste).toHaveTextContaining('New Sheriff in Town');
   });
-  it('should click on 10 minute of paste expiration ', () => {
-    SearchPage.pasteExpiration.click();
-    SearchPage.pasteExpiration10Minute.click();
+  it('should click on button Agree ', async () => {
+    await SearchPage.buttonAgreefirst.click();
   });
-  it('should add the value "how to gain dominance among developers"', () => {
-    SearchPage.pasteNameTitle.addValue('how to gain dominance among developers');
-    expect(SearchPage.pasteNameTitle).toHaveText('how to gain dominance among developers');
+  it('should click on "syntax Highlighting" ', async () => {
+    await SearchPage.syntaxHighlightingDropDown.click();
   });
-  it('should create new paste', () => {
-    SearchPage.createNewPaste.click();
+  it('should select "syntax Highlighting" - Bash', async () => {
+    await SearchPage.syntaxHighlightingBash.click();
   });
-  it('should check new paste for the existence ', () => {
-    //SearchPage.note.waitForDisplayed({ timeout: 5000 });
-    //expect(note).toHaveText('helloweb');
-    expect(browser).toHaveTitle('how to gain dominance among developers');
+  it('should click on 10 minute of paste expiration ', async () => {
+    await SearchPage.pasteExpiration.click();
+    await SearchPage.pasteExpiration10Minute.click();
   });
-  it('should has the syntax Bash ', () => {
-    //*[@class="btn -small h_800" and contains(text(), "Bash")]
-    expect(browser).toHaveTextContaining('Bash');
+  it('should add the value "how to gain dominance among developers"', async () => {
+    await SearchPage.pasteNameTitle.addValue('how to gain dominance among developers');
+    // await expect(SearchPage.pasteNameTitle).toHaveText('how to gain dominance among developers');
   });
-  it('should has code in text area', () => {
-    //*[@class="btn -small h_800" and contains(text(), "Bash")]
-    expect(browser).toHaveTextContaining('New Sheriff in Town');
+  it('should create new paste', async () => {
+    await SearchPage.createNewPaste.click();
+  });
+  it('should click on button AGREE', async () => {
+    await SearchPage.buttonAgreeSecond.click();
+  });
+
+  it('should check new paste for the existence ', async () => {
+    await expect(browser).toHaveTitleContaining('how to gain dominance among developers');
+  });
+  it('should has the syntax Bash ', async () => {
+    await expect(SearchPage.syntaxBash).toHaveTextContaining('Bash');
+  });
+  it('should has code in text area', async () => {
+    await expect(SearchPage.noteArea).toHaveTextContaining('New Sheriff in Town');
   });
 });
--- searchPage.js
+++ searchPage.js
@@ -4,11 +4,17 @@ class SearchPage extends OpenPage {
   get textNewPaste() {
     return $('#postform-text');
   }
+  get syntaxHighlightingDropDown() {
+    return $('#select2-postform-format-container');
+  }
+  get syntaxHighlightingBash() {
+    return $('//li[contains(text(), "Bash")]');
+  }
   get pasteExpiration() {
     return $('#select2-postform-expiration-container');
   }
   get pasteExpiration10Minute() {
-    return $('#select2-postform-expiration-result-q68m-10M');
+    return $('//li[contains(text(), "10 Minutes")]');
   }
   get pasteNameTitle() {
     return $('#postform-name');
@@ -16,9 +22,20 @@ class SearchPage extends OpenPage {
   get createNewPaste() {
     return $("button[type='submit']");
   }
-  get note() {
-    return $("div[class='info-top'] h1");
+  //button[normalize-space()='AGREE']
+  get buttonAgreefirst() {
+    return $('//button[normalize-space()="AGREE"]');
   }
+  get buttonAgreeSecond() {
+    return $('//button[normalize-space()="AGREE"]');
+  }
+  get syntaxBash() {
+    return $('//*[@class="btn -small h_800" and contains(text(), "Bash")]');
+  }
+  get noteArea() {
+    return $('//div[@class="de1"]');
+  }
+
   open() {
     super.open('https://pastebin.com/');
   }
